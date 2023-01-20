3af32ff1f506270cbfc319b0d805107f92e6c58f
Abhijeet Singh <abhijeetgovindsingh@gmail.com>
2023-01-20 15:53:24 +0000

    done with font end

--- routes/openaiRoutes.js
+++ routes/openaiRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
+const { generateImage } = require('../controllers/openaiController');
 const router = express.Router();
-const {generateImage} = require('../controllers/openaiController');
-router.post('/generateimage', generateImage)
-module.exports=router;
 
+router.post('/generateimage', generateImage);
+
+module.exports = router;
