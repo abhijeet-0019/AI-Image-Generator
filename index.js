065ad97b6e09038b066935612734bd4eaedf9a3b
Abhijeet Singh <abhijeetgovindsingh@gmail.com>
2023-01-20 15:34:03 +0000

    backend done

--- index.js
+++ index.js
@@ -6,4 +6,7 @@ const app = express();
 
 app.use('/openai', require('./routes/openaiRoutes'));
 
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
 app.listen(port, ()=> console.log(`Server is running at ${port}`));
